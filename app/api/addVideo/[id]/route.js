267e59ee8b32acf5ebef4d49213018a2f73367a7
shohag <shohagmiah2100@gmail.com>
2023-11-24 19:35:44 +0600

    not working

--- app/api/addVideo/[id]/route.js
+++ app/api/addVideo/[id]/route.js
@@ -1,4 +1,5 @@
 import { connectToDatabase } from "@/libs/connectToMongodb";
+import { Channel } from "@/models/channel";
 import { Video } from "@/models/video";
 import { NextResponse } from 'next/server';
 
@@ -7,6 +8,8 @@ export async function GET(req, { params }) {
     console.log('video id',id)
     try {
       await connectToDatabase();
+      // referencing channel model otherwise get error
+      Channel 
       const video = await Video.findOne({ _id: id.toString() }).populate('channelId');
       console.log(video)
       return new NextResponse({video},{message:"succesfull"});
