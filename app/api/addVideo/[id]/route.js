dc6ef25bc041a39def3c7fb1b78cfbfd22ecbf53
shohag <shohagmiah2100@gmail.com>
2023-11-24 19:01:07 +0600

    debugging video

--- app/api/addVideo/[id]/route.js
+++ app/api/addVideo/[id]/route.js
@@ -4,9 +4,10 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req, { params }) {
     const { id } = params;
+    console.log('video id',id)
     try {
       await connectToDatabase();
-      const video = await Video.findOne({ _id: id }).populate('channelId');
+      const video = await Video.findOne({ "_id": id }).populate('channelId');
       console.log(video)
       return new NextResponse(JSON.stringify(video));
     } catch (error) {
